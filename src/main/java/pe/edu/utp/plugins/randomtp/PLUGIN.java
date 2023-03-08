f6b1074ead45da414b6de7ac91c81fcb143657ca
Jonathan-MR19 <jonathanvegeta11@gmail.com>
2023-03-07 22:09:29 -0500

    Firstcommit

---
+++ src/main/java/pe/edu/utp/plugins/randomtp/PLUGIN.java
@@ -0,0 +1,17 @@
+package pe.edu.utp.plugins.randomtp;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PLUGIN extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
