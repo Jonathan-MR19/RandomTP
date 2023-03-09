55076cf436c5048b574377497cc2c6ac078307a3
Luis Bazan <luis@bazan.pe>
2023-03-09 00:01:58 -0500

    Hello command created

--- src/main/java/pe/edu/utp/plugins/randomtp/PLUGIN.java
+++ src/main/java/pe/edu/utp/plugins/randomtp/PLUGIN.java
@@ -1,13 +1,14 @@
 package pe.edu.utp.plugins.randomtp;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import pe.edu.utp.plugins.randomtp.commands.HelloCommand;
 
 public final class PLUGIN extends JavaPlugin {
 
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        this.getCommand("hello").setExecutor(new HelloCommand());
     }
 
     @Override
---
+++ src/main/java/pe/edu/utp/plugins/randomtp/commands/HelloCommand.java
@@ -0,0 +1,34 @@
+package pe.edu.utp.plugins.randomtp.commands;
+
+import org.bukkit.Location;
+import org.bukkit.block.Block;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class HelloCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        if(sender instanceof Player) {
+            Player jugador = (Player) sender;
+            sender.sendMessage("Eres un jugador");
+
+            // Location
+            Location loc = jugador.getLocation(); // Yellow block
+
+            // Cyan block - Floor
+            Location floor = loc.add(0, -1, 0);
+            Block cyanBlock = floor.getBlock();
+
+            jugador.sendMessage(
+                    String.format("El bloque donde estas parado es: %s", cyanBlock.getType())
+            );
+        } else {
+            sender.sendMessage("No eres un jugador");
+        }
+
+        return true;
+    }
+}
