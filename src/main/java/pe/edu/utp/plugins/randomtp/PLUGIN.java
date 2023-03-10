cf66e3bd0c539bf4a83e83691e3d760a03c26be9
Luis Bazan <luis@bazan.pe>
2023-03-09 23:44:28 -0500

    added config.yml

--- src/main/java/pe/edu/utp/plugins/randomtp/PLUGIN.java
+++ src/main/java/pe/edu/utp/plugins/randomtp/PLUGIN.java
@@ -8,9 +8,14 @@ public final class PLUGIN extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
+        this.saveDefaultConfig();
+
         HelloCommand helloCommand = new HelloCommand();
         this.getCommand("hello").setExecutor(helloCommand);
         this.getCommand("hello").setTabCompleter(helloCommand);
+
+        System.out.println("Valor de pvp");
+        System.out.println(getConfig().getBoolean("pvp"));
     }
 
     @Override
